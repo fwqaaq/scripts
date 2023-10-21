a778b26b4b60a48f11b90675377a5a5b97f740fb
fwqaaq <fwqaaq@gmail.com>
2023-10-21 16:17:59 +0800

    Update rdp_forward.sh about v6 -> v4

--- shell/rdp_forward.sh
+++ shell/rdp_forward.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-# 将 3389 端口转发到本机的 11111 端口
-# 杀死，ps -ux | grep socat
+# 将 3389 端口转发到本机的 11111 端口，如果公网只有 IPv6，
+# 则监听端使用 v4（tcp4-listen），而转发的流量应该是 v6（tcp6）
 socat tcp-listen:11111,reuseaddr,fork tcp4:192.168.50.7:3389 &
+# 杀死，ps -ux | grep socat
