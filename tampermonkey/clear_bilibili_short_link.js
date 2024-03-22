f63872c8d63fb8a8b3fbe1774caf9d4c0b59d14d
fwqaaq <fwqaaq@gmail.com>
2024-03-22 14:49:23 +0800

    Update clear_bilibili_short_link.js (#16)

--- tampermonkey/clear_bilibili_short_link.js
+++ tampermonkey/clear_bilibili_short_link.js
@@ -8,22 +8,27 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS', 
+  'Access-Control-Max-Age': '86400'
+}
+
 export default {
   /**
-   * 
    * @param {Request} request 
    * @param {*} env 
    * @param {ExecutionContext} ctx 
    * @returns
    */
   async fetch(request, env, ctx) {
-    if (request.method.toUpperCase() !== 'GET') return new Response("仅支持 GET 请求")
-    const bilibili = new URL(request.url).searchParams.get("bilibili")
-    if (!bilibili || !bilibili.includes("b23.tv")) return new Response("请发送带有正确 b 站短链的参数")
-    const res = await fetch(bilibili, { redirect: 'manual' })
+    if (request.method.toUpperCase() !== 'GET') return new Response("仅支持 GET 请求", {headers})
+    const tracked = new URL(request.url).searchParams.get("bilibili")
+    if(!tracked || !(tracked.includes("b23.tv") || tracked.includes("xhslink.com"))) return new Response("请发送带有正确跟踪短链的参数", {headers})
+    const res = await fetch(tracked, { redirect: 'manual' })
     const location = res.headers.get('location') ?? null
-    if (!location) return new Response("没有完整的 b 站链接，请查找输入是否正确")
+    if(!location) return new Response("没有完整的跟踪链接，请查找输入是否正确", {headers})
     const source = new URL(location)
-    return new Response(source.origin + source.pathname)
+    return new Response(source.origin + source.pathname, {headers})
   },
 };
