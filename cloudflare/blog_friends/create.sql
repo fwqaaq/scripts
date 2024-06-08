e80719ae7dbd1c322a2ec4b9d1c487b65abfc857
fwqaaq <fwqaaq@gmail.com>
2024-06-08 23:26:07 +0800

    delete cloudflare project

--- cloudflare/blog_friends/create.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS friends;
-CREATE TABLE IF NOT EXISTS friends (
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        url VARCHAR(255) NOT NULL,
-        avatar VARCHAR(255),
-        description TEXT
-);
